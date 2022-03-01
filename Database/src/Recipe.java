3d64f8b9ad1d3b2bfb342ae0c91ec8e251149e3c
ngDuyAnh <62583862+ngDuyAnh@users.noreply.github.com>
2022-03-01 17:40:42 -0600

    Recipe getter method

--- Database/src/Recipe.java
+++ Database/src/Recipe.java
@@ -1,4 +1,3 @@
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 /*
@@ -28,7 +27,16 @@ filterList - Linked list of string filter. Holiday like Christmas, New Year, to 
 
 Public method:
 Recipe() - Constructor to create an instance of recipe.
-getter methods...
+getName() - Get the name of the recipe.
+getNationality() - Get the nationality of the recipe.
+getPrepTime() - Get the prep time for the recipe.
+getCookTime() - Get the cooking time for the recipe.
+getCookingSkillLevel() - Get the cooking skill level for the recipe.
+getDescription() - Get the description of the recipe.
+getInstructionList() - Get the instructions for the recipe.
+getLink() - Get the link for the recipe tutorial.
+getVegetarian() - Get boolean flag if the recipe is a vegetarian recipe.
+getFilter() - Get the list of filters for the recipe.
 
 Future:
 Image of the recipe.
@@ -69,6 +77,96 @@ public class Recipe
         this.link = link;
         this.vegetarian = vegetarian;
         this.filterList = filterList;
+    }
+
+    /* getName()
+    Return:
+    The name of the recipe.
+    */
+    public String getName()
+    {
+        return this.name;
+    }
+
+    /* getNationality()
+    Return:
+    The nationality of the recipe.
+    */
+    public String getNationality()
+    {
+        return this.nationality;
+    }
+
+    /* getPrepTime()
+    Return:
+    Prep time for the recipe.
+    */
+    public int getPrepTime()
+    {
+        return this.prepTime;
+    }
+
+    /* getCookTime()
+    Return:
+    The cooking time for the recipe.
+    */
+    public int getCookTime()
+    {
+        return this.cookTime;
+    }
+
+    /* getCookingSkillLevel()
+    Return:
+    The cooking skill level for the recipe.
+    */
+    public String getCookingSkillLevel()
+    {
+        return this.cookingSkillLevel;
+    }
+
+    /* getDescription()
+    Return:
+    The description of the recipe.
+    */
+    public String getDescription()
+    {
+        return this.description;
+    }
+
+    /* getInstructionList()
+    Return:
+    The instructions for the recipe.
+    */
+    public ArrayList<String> getInstructionList()
+    {
+        return this.instructionList;
+    }
+
+    /* getLink()
+    Return:
+    The link for the recipe tutorial.
+    */
+    public String getLink()
+    {
+        return this.link;
+    }
+
+    /* getVegetarian()
+    Return:
+    Boolean flag if the recipe is a vegetarian recipe.
+    */
+    public boolean getVegetarian()
+    {
+        return this.vegetarian;
+    }
+
+    /* getFilter()
+    Return:
+    The list of filters for the recipe.
+    */
+    public ArrayList<String> getFilter()
+    {
+        return this.filterList;
     }
 
 }
