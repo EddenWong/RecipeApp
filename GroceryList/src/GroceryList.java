054ce11b1822e97954abe47d31fd72424058e9ae
ngDuyAnh <62583862+ngDuyAnh@users.noreply.github.com>
2022-03-02 19:48:47 -0600

    GroceryList finalized and tested.

--- GroceryList/src/GroceryList.java
+++ GroceryList/src/GroceryList.java
@@ -12,39 +12,82 @@ Public method:
 GroceryList() - Constructor to initialize the linked list.
 addIngredient() - Add the given string ingredient to the list.
 removeIngredient() - Remove the given index of the ingredient from the list.
-getIngredient() - Get the ingredient from a given index
+getIngredient() - Get the ingredient from a given index.
+numIngredients() - Number of ingredients in the grocery list.
 */
 
 import java.util.LinkedList;
 
 public class GroceryList
 {
+    // Private member
     private final LinkedList<String> groceryList;
 
+    // Public method
+
+    /* GroceryList()
+    */
     public GroceryList()
     {
-        groceryList = new LinkedList<>();
+        groceryList = new LinkedList<String>();
     }
 
+    /* addIngredient()
+    Parameter:
+    newIngredient - The given ingredient string to append to the grocery list.
+    */
     public void addIngredient(String newIngredient)
     {
-        groceryList.add(newIngredient);
+        if (newIngredient != null)
+        {
+            groceryList.add(newIngredient);
+        }
     }
 
+    /* removeIngredient()
+    Parameter:
+    index - Remove the ingredient at given index. Do nothing if element at given
+        index does not exist.
+    */
     public void removeIngredient(int index)
     {
-        if(groceryList.size() > index && index >= 0)
+        // Pre-condition
+        assert(index >= 0 && index < this.groceryList.size());
+
+        // Remove the ingredient at given index
+        if(index < groceryList.size() && index >= 0)
         {
             groceryList.remove(index);
         }
     }
 
-    public String getIngredient(int index) {
+    /* getIngredient()
+    Parameter:
+    index - The index of the ingredient in the grocery list.
+
+    Return:
+    String ingredient at given index in the grocery list.
+    Null if the given index is invalid.
+    */
+    public String getIngredient(int index)
+    {
+        // Pre-condition
+        assert(index >= 0 && index < this.groceryList.size());
+
+        // Get the ingredient string from the grocery list
         String returnIngredient = null;
-        if (groceryList.size() > index && index >= 0) {
+        if (index < groceryList.size() && index >= 0)
+        {
             returnIngredient = groceryList.get(index);
         }
 
+        // Return
         return returnIngredient;
     }
+
+    public int numIngredients()
+    {
+        return this.groceryList.size();
+    }
+
 }
--- GroceryList/src/GroceryListTest.java
+++ GroceryList/src/GroceryListTest.java
@@ -7,7 +7,6 @@ class GroceryListTest {
     {
         //Assert that a new grocery list is empty
         GroceryList addTest = new GroceryList();
-        assert(addTest.getIngredient(0) == null);
 
         String newIngredient0 = "Flour";
         String newIngredient1 = "Sugar";
@@ -16,22 +15,24 @@ class GroceryListTest {
         //Assert that the added ingredient is correctly added
         addTest.addIngredient(newIngredient0);
         assert(addTest.getIngredient(0).equals(newIngredient0));
+        assert (addTest.numIngredients() == 1);
 
         //Assert that the new ingredient to the next index
         addTest.addIngredient(newIngredient1);
         assert(addTest.getIngredient(0).equals(newIngredient0));
         assert(addTest.getIngredient(1).equals(newIngredient1));
+        assert (addTest.numIngredients() == 2);
 
         //Assert that the new ingredient to the next index
         addTest.addIngredient(newIngredient2);
         assert(addTest.getIngredient(0).equals(newIngredient0));
         assert(addTest.getIngredient(1).equals(newIngredient1));
         assert(addTest.getIngredient(2).equals(newIngredient2));
+        assert (addTest.numIngredients() == 3);
 
-        //Assert that null strings accepted
+        //Assert that null strings rejected
         addTest.addIngredient(null);
-        assert(addTest.getIngredient(3) == null);
-
+        assert(addTest.numIngredients() == 3);
     }
 
     @org.junit.jupiter.api.Test
@@ -39,7 +40,6 @@ class GroceryListTest {
     {
         //Assert that a new grocery list is empty
         GroceryList removeTest = new GroceryList();
-        assert(removeTest.getIngredient(0) == null);
 
         //Add three ingredients
         String newIngredient0 = "Flour";
@@ -54,18 +54,15 @@ class GroceryListTest {
         removeTest.removeIngredient(1);
         assert(removeTest.getIngredient(0).equals(newIngredient0));
         assert(removeTest.getIngredient(1).equals(newIngredient2));
-        assert(removeTest.getIngredient(2) == (null));
+        assert(removeTest.numIngredients() == 2);
 
         //Assert that the ingredient in index 0 was removed
         removeTest.removeIngredient(0);
         assert(removeTest.getIngredient(0).equals(newIngredient2));
-        assert(removeTest.getIngredient(1) == null);
-        assert(removeTest.getIngredient(2) == null);
+        assert(removeTest.numIngredients() == 1);
 
         //Assert that all ingredients are removed
         removeTest.removeIngredient(0);
-        assert(removeTest.getIngredient(0) == null);
-        assert(removeTest.getIngredient(1) == null);
-        assert(removeTest.getIngredient(2) == null);
+        assert(removeTest.numIngredients() == 0);
     }
 }
