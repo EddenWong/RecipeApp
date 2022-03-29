7aeb394d934acbb437719718287f55de639b3bd3
minh981205 <minhvip10x@gmail.com>
2022-03-29 12:29:08 -0500

    Added table for GroceryList. Added GroceryListInterface and its general implementation

---
+++ app/src/main/java/persistence/GroceryListInterface.java
@@ -0,0 +1,16 @@
+package persistence;
+
+import java.util.List;
+
+import objects.GroceryList;
+
+public interface GroceryListInterface {
+
+    GroceryList getGroceryList();
+
+    String insertItem(String currentItem);
+
+    String updateItem(String currentItem);
+
+    void deleteItem(String currentItem);
+}
