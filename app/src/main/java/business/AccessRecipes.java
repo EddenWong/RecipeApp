9335c201d39256a5f7f5f256579c99fab40f667d
minh981205 <minhvip10x@gmail.com>
2022-03-30 04:12:59 -0500

    Provide function to get all recipes or a specific recipe from db

--- app/src/main/java/business/AccessRecipes.java
+++ app/src/main/java/business/AccessRecipes.java
@@ -1,7 +1,9 @@
-package tests.business;
+package business;
 
+import java.util.Collections;
 import java.util.List;
 
+import application.Services;
 import objects.Recipe;
 import persistence.RecipePersistence;
 
@@ -12,5 +14,42 @@ public class AccessRecipes {
     private int currentRecipe;
 
     public AccessRecipes() {
+        recipePersistence = Services.getRecipePersistence();
+        recipes = null;
+        recipe = null;
+        currentRecipe = 0;
     }
+
+    public AccessRecipes(final RecipePersistence recipePersistence) {
+        this();
+        this.recipePersistence = recipePersistence;
+    }
+
+    public List<Recipe> getRecipes()
+    {
+        recipes = recipePersistence.getRecipes();
+        return Collections.unmodifiableList(recipes);
+    }
+
+    public Recipe getSequential()
+    {
+        if (recipes == null)
+        {
+            recipes = recipePersistence.getRecipes();
+            currentRecipe = 0;
+        }
+        if (currentRecipe < recipes.size())
+        {
+            recipe = recipes.get(currentRecipe);
+            currentRecipe++;
+        }
+        else
+        {
+            recipes = null;
+            recipe = null;
+            currentRecipe = 0;
+        }
+        return recipe;
+    }
+
 }
