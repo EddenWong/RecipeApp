e41d2dc7290e9ba2166c7da885a902cf81dcfc6d
minh981205 <minhvip10x@gmail.com>
2022-03-30 06:34:58 -0500

    Create tests to test the functions to access the recipes in db. Will need to implement more tests to cover all cases

--- app/src/test/java/tests/business/AccessRecipesIT.java
+++ app/src/test/java/tests/business/AccessRecipesIT.java
@@ -1,4 +1,8 @@
-package comp3350.srsys.tests.business;
+package tests.business;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.After;
 import org.junit.Before;
@@ -6,71 +10,79 @@ import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
-import comp3350.srsys.application.Main;
-import comp3350.srsys.business.AccessCourses;
-import comp3350.srsys.objects.Course;
-import comp3350.srsys.persistence.CoursePersistence;
-import comp3350.srsys.persistence.hsqldb.CoursePersistenceHSQLDB;
-import comp3350.srsys.tests.utils.TestUtils;
+import business.AccessRecipes;
+import objects.Recipe;
+import persistence.RecipePersistence;
+import persistence.hsqldb.RecipePersistenceHSQLDB;
+import tests.utils.TestUtils;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-
-public class AccessCoursesIT {
-    private AccessCourses accessCourses;
+public class AccessRecipesIT {
+    private AccessRecipes accessRecipes;
     private File tempDB;
 
 
     @Before
     public void setUp() throws IOException {
         this.tempDB = TestUtils.copyDB();
-        final CoursePersistence persistence = new CoursePersistenceHSQLDB(this.tempDB.getAbsolutePath().replace(".script", ""));
-        this.accessCourses = new AccessCourses(persistence);
+        final RecipePersistence persistence = new RecipePersistenceHSQLDB(this.tempDB.getAbsolutePath().replace(".script", ""));
+        this.accessRecipes = new AccessRecipes(persistence);
     }
 
     @Test
-    public void testListCourses() {
-        final Course course;
+    public void testListRecipes() {
+        final Recipe recipe;
 
-        course = accessCourses.getSequential();
-        assertNotNull("first sequential course should not be null", course);
-        assertTrue("COMP3010".equals(course.getCourseID()));
+        recipe = accessRecipes.getSequential();
+        assertNotNull("first sequential recipe should not be null", recipe);
+        assertTrue(1 == (recipe.getRecipeID()));
 
-        System.out.println("Finished test AccessCourses");
+        System.out.println("Finished test AccessRecipes");
     }
 
     @Test
-    public void testGetCourses() {
-        final List<Course> courses = accessCourses.getCourses();
-        assertEquals(4, courses.size());
+    public void testGetRecipes() {
+        final List<Recipe> recipes = accessRecipes.getRecipes();
+        assertEquals(5, recipes.size());
     }
 
     @Test
-    public void testDeleteCourse() {
-        final Course c = accessCourses.getSequential();
-        List<Course> courses = accessCourses.getCourses();
-        assertEquals(4, courses.size());
-        accessCourses.deleteCourse(c);
-        courses = accessCourses.getCourses();
-        assertEquals(3, courses.size());
+    public void testDeleteRecipe() {
+        final Recipe res = accessRecipes.getSequential();
+        List<Recipe> recipes = accessRecipes.getRecipes();
+        assertEquals(5, recipes.size());
+        accessRecipes.deleteRecipe(res);
+        recipes = accessRecipes.getRecipes();
+        assertEquals(4, recipes.size());
     }
 
     @Test
-    public void testInsertCourse() {
-        final Course c = new Course("3190", "AI");
-        accessCourses.insertCourse(c);
-        assertEquals(5, accessCourses.getCourses().size());
+    public void testInsertRecipe() {
+        ArrayList<String> ingredientList = new ArrayList<>();
+        ingredientList.add("Random ingred");
+
+        ArrayList<String> categoryList = new ArrayList<>();
+        categoryList.add("Random Category");
+
+        final Recipe res = new Recipe(10, "Test Beef", "US", ingredientList, 5, 10, "Easy", "Description for recipe", "Instruction for recipe", "random link", categoryList);
+        accessRecipes.insertRecipe(res);
+        assertEquals(6, accessRecipes.getRecipes().size());
     }
 
     @Test
-    public void testUpdateCourse() {
-        final Course c = accessCourses.getSequential();
-        final Course u = new Course(c.getCourseID(), "A new name");
-        accessCourses.updateCourse(u);
-        assertEquals(4, accessCourses.getCourses().size());
+    public void testUpdateRecipe() {
+        ArrayList<String> ingredientList = new ArrayList<>();
+        ingredientList.add("Random ingred");
+
+        ArrayList<String> categoryList = new ArrayList<>();
+        categoryList.add("Random Category");
+
+        final Recipe res = accessRecipes.getSequential();
+        final Recipe res2 = new Recipe(res.getRecipeID(), "Test Beef", "US", ingredientList, 5, 10, "Easy", "Description for recipe", "Instruction for recipe", "random link", categoryList);
+        accessRecipes.updateRecipe(res2);
+        assertEquals(5, accessRecipes.getRecipes().size());
     }
 
     @After
