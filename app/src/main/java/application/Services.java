6847a2df7c2019db51d74c64f1132d17f4416c6c
minh981205 <minhvip10x@gmail.com>
2022-03-30 04:11:26 -0500

    Provide a way for business layer to access persistence layer Recipe class

--- app/src/main/java/application/Services.java
+++ app/src/main/java/application/Services.java
@@ -1,46 +1,20 @@
-package comp3350.srsys.application;
+package application;
 
-import comp3350.srsys.persistence.CoursePersistence;
-import comp3350.srsys.persistence.SCPersistence;
-import comp3350.srsys.persistence.StudentPersistence;
-import comp3350.srsys.persistence.hsqldb.CoursePersistenceHSQLDB;
-import comp3350.srsys.persistence.hsqldb.SCPersistenceHSQLDB;
-import comp3350.srsys.persistence.hsqldb.StudentPersistenceHSQLDB;
+import persistence.RecipePersistence;
+import persistence.hsqldb.RecipePersistenceHSQLDB;
 
 public class Services
 {
-	private static StudentPersistence studentPersistence = null;
-	private static CoursePersistence coursePersistence = null;
-	private static SCPersistence scPersistence = null;
+	private static RecipePersistence recipePersistence = null;
 
-	public static synchronized StudentPersistence getStudentPersistence()
+	public static synchronized RecipePersistence getRecipePersistence()
     {
-		if (studentPersistence == null)
+		if (recipePersistence == null)
 		{
-		    //studentPersistence = new StudentPersistenceStub();
-            studentPersistence = new StudentPersistenceHSQLDB(Main.getDBPathName());
+		    //recipePersistence = new RecipePersistenceStub();
+            recipePersistence = new RecipePersistenceHSQLDB(Main.getDBPathName());
         }
 
-        return studentPersistence;
+        return recipePersistence;
 	}
-
-    public static synchronized CoursePersistence getCoursePersistence()
-    {
-        if (coursePersistence == null)
-        {
-            // coursePersistence = new CoursePersistenceStub();
-            coursePersistence = new CoursePersistenceHSQLDB(Main.getDBPathName());
-        }
-
-        return coursePersistence;
-    }
-
-	public static synchronized SCPersistence getScPersistence() {
-        if (scPersistence == null) {
-            // scPersistence = new SCPersistenceStub();
-            scPersistence = new SCPersistenceHSQLDB(Main.getDBPathName());
-        }
-
-        return scPersistence;
-    }
 }
