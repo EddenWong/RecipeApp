226cdea427909d4fbe7d0da654faa6a8145be2a3
minh981205 <minhvip10x@gmail.com>
2022-04-01 02:24:39 -0500

    Added a class in business layer to access the GroceryList in persistent layer

--- app/src/main/java/business/AccessGroceryList.java
+++ app/src/main/java/business/AccessGroceryList.java
@@ -1,11 +1,10 @@
 package business;
 
+import java.util.Collections;
 import java.util.List;
 
 import application.Services;
-import objects.GroceryList;
 import persistence.GroceryListPersistence;
-import persistence.RecipePersistence;
 
 public class AccessGroceryList {
     private GroceryListPersistence groceryListPersistence;
@@ -25,5 +24,39 @@ public class AccessGroceryList {
         this.groceryListPersistence = groceryListPersistence;
     }
 
+    public List<String> getGroceryList()
+    {
+        groceryList = groceryListPersistence.getGroceryList();
+        return Collections.unmodifiableList(groceryList);
+    }
 
+    public String getSequential() {
+        if (groceryList == null) {
+            groceryList = groceryListPersistence.getGroceryList();
+            currentItem = 0;
+        }
+        if (currentItem < groceryList.size()) {
+            item = groceryList.get(currentItem);
+            currentItem++;
+        } else {
+            groceryList = null;
+            item = null;
+            currentItem = 0;
+        }
+        return item;
+    }
+
+    public String insertItem(String currentItem)
+    {
+        return groceryListPersistence.insertItem(currentItem);
+    }
+
+    public String updateItem(String currentItem, String newItem)
+    {
+        return groceryListPersistence.updateItem(currentItem, newItem);
+    }
+
+    public void deleteItem(String currentItem) {
+        groceryListPersistence.deleteItem(currentItem);
+    }
 }
