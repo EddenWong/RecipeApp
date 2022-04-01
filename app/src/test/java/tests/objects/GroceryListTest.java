21b57fca8c94a482a386faa9b80925e1cfa0ef9a
minh981205 <minhvip10x@gmail.com>
2022-04-01 02:30:42 -0500

    Added unit test for GroceryListTest

--- app/src/test/java/tests/objects/GroceryListTest.java
+++ app/src/test/java/tests/objects/GroceryListTest.java
@@ -14,37 +14,37 @@ import objects.GroceryList;
 public class GroceryListTest{
 
    @Test
-   public void groceryTest1{
+   public void groceryTest1() {
+       GroceryList Grocery;
+       System.out.println("\nStarting Grocery\n");
+       ArrayList<String> groceryItems = new ArrayList<String>();
+       groceryItems.add("sugar");
+       groceryItems.add("salt");
+       groceryItems.add("oil");
 
-    GroceryList Grocery;
 
-    System.out.println("\nStarting Grocery\n");
+//       assertNull(Grocery);
+       Grocery = new GroceryList(groceryItems);
 
-    ArrayList<String> groceryItems = new ArrayList<String>();
-    groceryItems.add("sugar");
-    groceryItems.add("salt");
-    groceryItems.add("oil");
 
-    assertNull(Grocery);
-    Grocery = new GroceryList(groceryItems);
-    //check if the object is not null after creation
-    assertNotNull(Grocery);
-    assertTrue(this.groceryItems.equals(Grocery.getGroceryList));
-    
-    ArrayList<String> groceryItems2 = new ArrayList<String>();
-    groceryItems.add("pepper");
-    groceryItems.add("spice");
+       //check if the object is not null after creation
+       assertNotNull(Grocery);
+       assertTrue(groceryItems.equals(Grocery.getGroceryList()));
 
-    assertFalse(this.groceryItems2.equals(Grocery.getGroceryList));
-    assertFalse(this.groceryItems2.equals(groceryItems));
+       ArrayList<String> groceryItems2 = new ArrayList<String>();
+       groceryItems2.add("pepper");
+       groceryItems2.add("spice");
 
-    groceryItems2.removeAll(groceryItems2);
-    groceryItems2.add("sugar");
-    groceryItems2.add("salt");
-    groceryItems2.add("oil");
-    assertTrue(this.groceryItems2.equals(Grocery.getGroceryList));
+       assertFalse(groceryItems2.equals(Grocery.getGroceryList()));
+       assertFalse(groceryItems2.equals(groceryItems));
 
-    System.out.println("\nEnding Grocery Test\n");
+       groceryItems2.removeAll(groceryItems2);
+       groceryItems2.add("sugar");
+       groceryItems2.add("salt");
+       groceryItems2.add("oil");
+       assertTrue(groceryItems2.equals(Grocery.getGroceryList()));
+
+       System.out.println("\nEnding Grocery Test\n");
 
    }
 
