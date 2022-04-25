5a12ebc32d436b4d84081692cd8f992d7dd7d194
Duy Anh Nguyen <In_Drop@outlook.com>
2022-04-25 14:30:57 -0500

    Home screen acceptance test.

---
+++ app/src/androidTest/java/presentation/HomeActivityTest.java
@@ -0,0 +1,55 @@
+
+import androidx.test.espresso.DataInteraction;
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import static androidx.test.InstrumentationRegistry.getInstrumentation;
+import static androidx.test.espresso.Espresso.onData;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.pressBack;
+import static androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
+import static androidx.test.espresso.action.ViewActions.*;
+import static androidx.test.espresso.assertion.ViewAssertions.*;
+import static androidx.test.espresso.matcher.ViewMatchers.*;
+
+import com.example.recipeapp.R;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.Matchers.is;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class HomeActivityTest {
+
+    @Rule
+    public ActivityTestRule<HomeActivity> mActivityTestRule = new ActivityTestRule<>(HomeActivity.class);
+
+    @Test
+    public void homeActivityTest() {
+        ViewInteraction button = onView(
+allOf(withId(R.id.button), withText("GROCERY LIST"),
+withParent(withParent(IsInstanceOf.<View>instanceOf(android.view.ViewGroup.class))),
+isDisplayed()));
+        button.check(matches(isDisplayed()));
+        
+        ViewInteraction button2 = onView(
+allOf(withId(R.id.button2), withText("RECIPE LIST"),
+withParent(withParent(IsInstanceOf.<View>instanceOf(android.view.ViewGroup.class))),
+isDisplayed()));
+        button2.check(matches(isDisplayed()));
+        }
+    }
