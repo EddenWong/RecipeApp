aca40216e21a99ff1d16aaf3b5de3f48340305b4
Duy Anh Nguyen <In_Drop@outlook.com>
2022-04-25 14:35:16 -0500

    Acceptance test Home Activity built and no error

--- app/src/androidTest/java/presentation/HomeActivityTest.java
+++ app/src/androidTest/java/presentation/HomeActivityTest.java
@@ -1,35 +1,28 @@
+package presentation;
 
-import androidx.test.espresso.DataInteraction;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withParent;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+import android.view.View;
+
 import androidx.test.espresso.ViewInteraction;
 import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.runner.AndroidJUnit4;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.ViewParent;
-
-import static androidx.test.InstrumentationRegistry.getInstrumentation;
-import static androidx.test.espresso.Espresso.onData;
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.Espresso.pressBack;
-import static androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
-import static androidx.test.espresso.action.ViewActions.*;
-import static androidx.test.espresso.assertion.ViewAssertions.*;
-import static androidx.test.espresso.matcher.ViewMatchers.*;
 
 import com.example.recipeapp.R;
 
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
-import org.hamcrest.TypeSafeMatcher;
 import org.hamcrest.core.IsInstanceOf;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.anything;
-import static org.hamcrest.Matchers.is;
+import presentation.HomeActivity;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
@@ -41,15 +34,15 @@ public class HomeActivityTest {
     @Test
     public void homeActivityTest() {
         ViewInteraction button = onView(
-allOf(withId(R.id.button), withText("GROCERY LIST"),
-withParent(withParent(IsInstanceOf.<View>instanceOf(android.view.ViewGroup.class))),
-isDisplayed()));
+                allOf(withId(R.id.button), withText("GROCERY LIST"),
+                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.view.ViewGroup.class))),
+                        isDisplayed()));
         button.check(matches(isDisplayed()));
-        
+
         ViewInteraction button2 = onView(
-allOf(withId(R.id.button2), withText("RECIPE LIST"),
-withParent(withParent(IsInstanceOf.<View>instanceOf(android.view.ViewGroup.class))),
-isDisplayed()));
+                allOf(withId(R.id.button2), withText("RECIPE LIST"),
+                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.view.ViewGroup.class))),
+                        isDisplayed()));
         button2.check(matches(isDisplayed()));
-        }
     }
+}
